bbcfa5a3648658382153d2c22cba07c32c74136b
茉灵智库 <111191349+88lin@users.noreply.github.com>
2026-03-22 03:54:04 +0800

    Update video_vip.user.js

--- video_vip.user.js
+++ video_vip.user.js
@@ -132,7 +132,7 @@ const superVip = (function () {
         autoPlayerVal: "auto_player_value_" + window.location.host,
         videoParseList: [
             {"name": "优质A", "type": "1,3", "url": "https://json.fongmi.cc/web?url="},
-            {"name": "优质B", "type": "1,3", "url": "https://www.ckplayer.vip/jiexi/?url="},
+            {"name": "CK解析", "type": "1,3", "url": "https://www.ckplayer.vip/jiexi/?url="},
 			{"name": "Player-JY", "type": "1,3", "url": "https://jx.playerjy.com/?url="},
             {"name": "虾米解析", "type": "1,3", "url": "https://jx.xmflv.com/?url="},
             {"name": "789解析", "type": "1,3", "url": "https://jiexi.789jiexi.icu:4433/?url="},
@@ -141,7 +141,6 @@ const superVip = (function () {
 			{"name": "冰豆解析", "type": "1,3", "url": "https://bd.jx.cn/?url="},
 			{"name": "剖元解析", "type": "1,3", "url": "https://www.pouyun.com/?url="},
             {"name": "973解析", "type": "1,3", "url": "https://jx.973973.xyz/?url="},
-            {"name": "CK", "type": "1,3", "url": "https://www.ckplayer.vip/jiexi/?url="},
             {"name": "七哥解析", "type": "1,3", "url": "https://jx.nnxv.cn/tv.php?url="},
             {"name": "盘古", "type": "1,3", "url": "https://www.pangujiexi.com/jiexi/?url="},
             {"name": "playm3u8", "type": "1,3", "url": "https://www.playm3u8.cn/jiexi.php?url="},
